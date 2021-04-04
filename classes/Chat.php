30fef8bcf72e563c124b65f511760f09f5961bed
Jin8-Nikolay <67728795+Jin8-Nikolay@users.noreply.github.com>
2021-04-04 20:22:41 +0300

    Add sending messages

--- classes/Chat.php
+++ classes/Chat.php
@@ -15,12 +15,48 @@ class Chat
         }
         $key = $headers['Sec-WebSocket-Key'];
         $sKey = base64_encode(pack('H*', sha1($key . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11')));
+
         $strHeadr = "HTTP/1.1 101 Switching Protocols \r\n" .
             "Upgrade: websocket\r\n" .
             "Connection: Upgrade\r\n" .
             "WebSocket-Origin: $host\r\n" .
             "WebSocket-Location: ws://$host:$port/websocket/server.php\r\n" .
             "Sec-WebSocket-Accept:$sKey\r\n\r\n";
+
         socket_write($newSocket, $strHeadr, strlen($strHeadr));
+    }
+
+    public function newConnectionACK($client_ip_address)
+    {
+        $message = "New client " . $client_ip_address . " connected";
+        $messageArray = [
+            "message" => $message,
+            "type" => "newConnectionACK"
+        ];
+        $ask = $this->seal(json_encode($messageArray));
+        return $ask;
+    }
+
+    public function seal($socketData)
+    {
+        $b1 = 0x81;
+        $length = strlen($socketData);
+        $header = "";
+        if ($length <= 125) {
+            $header = pack('CC', $b1, $length);
+        } elseif ($length > 125 && $length < 65535) {
+            $header = pack('CCn', $b1, 126, $length);
+        } elseif ($length > 65535) {
+            $header = pack('CCNN', $b1, 127, $length);
+        }
+        return $header . $socketData;
+    }
+
+    public function send($message, $clientSocketArray)
+    {
+        $messageLength = strlen($message);
+        foreach ($clientSocketArray as $clientSocket){
+            @socket_write($clientSocket, $message, $messageLength);
+        }
     }
 }
--- server.php
+++ server.php
@@ -5,14 +5,32 @@ require_once "classes/Chat.php";
 $chat = new Chat();
 
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+
 socket_set_option($socket, SOL_SOCKET, SO_REUSEADDR, 1);
 socket_bind($socket, 0, PORT);
+
 socket_listen($socket);
 
+$clientSocketArray = [$socket];
+
 while (true) {
-    $newSocket = socket_accept($socket);
-    $header = socket_read($newSocket, 1024);
-    $chat->sendHeaders($header, $newSocket, '"localhost/websocket"', PORT);
+
+    $newSocketArray = $clientSocketArray;
+    $null = [];
+    socket_select($newSocketArray, $null, $null, 0, 10);
+
+    if (in_array($socket, $newSocketArray)) {
+        $newSocket = socket_accept($socket);
+        $clientSocketArray[] = $newSocket;
+
+        $header = socket_read($newSocket, 1024);
+        $chat->sendHeaders($header, $newSocket, '"localhost/websocket"', PORT);
+
+        socket_getpeername($newSocket, $client_ip_address);
+        $connectionACK = $chat->newConnectionACK($client_ip_address);
+        $chat->send($connectionACK, $clientSocketArray);
+
+    }
 }
 
 socket_close($socket);
